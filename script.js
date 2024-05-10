729d3526258893be420f1977d45bb4a650bb6923
KeithYao1012 <147405253+KeithYao1012@users.noreply.github.com>
2024-05-10 15:08:14 -0400

    First commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function showAnswer(answerId) {
+    var answer = document.getElementById(answerId);
+    if (answer.style.display === "none") {
+        answer.style.display = "block";
+    } else {
+        answer.style.display = "none";
+    }
+}
